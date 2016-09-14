691cf60920647a3f9c171db5ae728cdaaed0a2b1
PuntusovAleksandr <puntusov.aleksandr@gmail.com>
2016-09-14 17:30:02 +0400

    added select theme

--- app/src/main/java/com/lucerotech/aleksandrp/w8monitor/login/SplashActivity.java
+++ app/src/main/java/com/lucerotech/aleksandrp/w8monitor/login/SplashActivity.java
@@ -1,12 +1,16 @@
 package com.lucerotech.aleksandrp.w8monitor.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.lucerotech.aleksandrp.w8monitor.R;
+import com.lucerotech.aleksandrp.w8monitor.utils.SettingsApp;
 
 /**
  * Created by AleksandrP on 13.09.2016.
@@ -20,6 +24,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SettingsApp.FILE_NAME, Context.MODE_PRIVATE);
+        boolean themeDark = SettingsApp.isThemeDark(sharedPreferences);
+        int modeNightNo = 0;
+        if (themeDark) {
+            modeNightNo = AppCompatDelegate.MODE_NIGHT_NO;
+        } else {
+            modeNightNo = AppCompatDelegate.MODE_NIGHT_YES;
+        }
+        AppCompatDelegate.setDefaultNightMode(modeNightNo);
+
         // init handler
         mHandlerStart = new Handler();
         Runnable startRun = initRunableStart();
@@ -35,12 +50,12 @@ public class SplashActivity extends AppCompatActivity {
     @NonNull
     private Runnable initRunableStart() {
         return new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    SplashActivity.this.finish();
-                }
-            };
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
     }
 }
--- app/src/main/java/com/lucerotech/aleksandrp/w8monitor/utils/SettingsApp.java
+++ app/src/main/java/com/lucerotech/aleksandrp/w8monitor/utils/SettingsApp.java
@@ -16,12 +16,25 @@ public class SettingsApp {
 
     // Keys for opening settings from xml file
     private static final String KEY_AUTO_LOGIN = "key_auto_login";
+    private static final String KEY_SELECT_THEME = "KEY_SELECT_THEME";
 
     // Default values of settings
     private static final String DEF_EMPTY_STRING = "";
     private static final boolean DEF_EMPTY_BOOLEAN = false;
+    private static final boolean DEF_NOT_EMPTY_BOOLEAN = true;
 
 
+
+    public static boolean isThemeDark(SharedPreferences preferences) {
+        return preferences.getBoolean(KEY_SELECT_THEME, DEF_NOT_EMPTY_BOOLEAN);
+    }
+
+
+    public static void setThemeDark(boolean ip, SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(KEY_SELECT_THEME, ip);
+        editor.commit();
+    }
 
     public static boolean getAutoLogin(SharedPreferences preferences) {
         return preferences.getBoolean(KEY_AUTO_LOGIN, DEF_EMPTY_BOOLEAN);
