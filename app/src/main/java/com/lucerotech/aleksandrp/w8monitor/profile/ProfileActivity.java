fb1a6214a77fbda34b4c9c50cf0949c7f62106de
PuntusovAleksandr <puntusov.aleksandr@gmail.com>
2016-09-16 13:03:20 +0400

    start make profile

--- app/src/main/java/com/lucerotech/aleksandrp/w8monitor/profile/ProfileActivity.java
+++ app/src/main/java/com/lucerotech/aleksandrp/w8monitor/profile/ProfileActivity.java
@@ -5,11 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.lucerotech.aleksandrp.w8monitor.R;
 
+import butterknife.ButterKnife;
+
 public class ProfileActivity extends AppCompatActivity {
+
+
+//    @Bind(R.id.ib_login)
+//    ImageView ib_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+
     }
 }
