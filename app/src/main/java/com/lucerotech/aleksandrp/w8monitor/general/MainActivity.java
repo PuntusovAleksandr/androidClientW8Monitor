98cb8fde3ba870534b2a1d2b1a8d44dd9ffb45fc
PuntusovAleksandr <puntusov.aleksandr@gmail.com>
2016-09-21 09:47:39 +0400

    added mainActivity

---
+++ app/src/main/java/com/lucerotech/aleksandrp/w8monitor/general/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lucerotech.aleksandrp.w8monitor.general;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lucerotech.aleksandrp.w8monitor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
