8798fd0fb86a3f267f51187007d36c911290ece3
PuntusovAleksandr <puntusov.aleksandr@gmail.com>
2016-12-01 10:38:30 +0400

    синхронизированно с релизной версией без апи от 01,12

--- app/src/main/java/com/lucertech/w8monitor/android/ble/BluetoothHandler.java
+++ app/src/main/java/com/lucertech/w8monitor/android/ble/BluetoothHandler.java
@@ -554,7 +554,6 @@ public class BluetoothHandler {
         return scanCallback;
     }
 
-
     // scan device
     public void scanLeDevice(boolean enable) {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
