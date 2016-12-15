f6b51b66dce9330073fedc50b9f29f9f33501819
PuntusovAleksandr <puntusov.aleksandr@gmail.com>
2016-12-15 19:14:31 +0200

    make sound wav

--- app/src/main/java/com/w8/w8monitor/android/utils/PlaySound.java
+++ app/src/main/java/com/w8/w8monitor/android/utils/PlaySound.java
@@ -1,18 +1,14 @@
 package com.w8.w8monitor.android.utils;
 
-import android.app.NotificationManager;
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Handler;
-import android.support.v4.app.NotificationCompat;
 
 import com.w8.w8monitor.android.App;
 import com.w8.w8monitor.android.R;
 
-import static com.facebook.FacebookSdk.getApplicationContext;
+import java.io.IOException;
 
 /**
  * Created by AleksandrP on 15.12.2016.
@@ -26,21 +22,43 @@ public class PlaySound {
         Context context = App.getContext();
 
         Uri ringtone = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.one_heart_bit);
-        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), ringtone);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-//                r.play();
+//        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), ringtone);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+////                r.play();
+//            }
+//        }, 300);
+
+
+        MediaPlayer mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, ringtone);
+        } catch (IOException mE) {
+            mE.printStackTrace();
+        }
+        final AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+            mMediaPlayer.setLooping(true);
+            try {
+                mMediaPlayer.prepare();
+            } catch (IOException mE) {
+                mE.printStackTrace();
             }
-        }, 300);
+            mMediaPlayer.start();
+        }
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
-                .setContentTitle(null)
-                .setContentText(null)
-                .setSound(ringtone); //This sets the sound to play
 
-        //Display notification
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, mBuilder.build());
+
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
+//                .setContentTitle(null)
+//                .setContentText(null)
+//                .setSound(ringtone); //This sets the sound to play
+//
+//        //Display notification
+//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.notify(0, mBuilder.build());
     }
 }
